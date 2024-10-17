1bb7855bce48a513658f867978deaff0f4e1ab01
HamalainenTaru <thamalainen1990@gmail.com>
2024-10-17 13:18:10 +0300

    created user model

---
+++ server/models/user.model.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      trim: true,
+      unique: true,
+      match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, "Invalid email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.set("toJSON", {
+  transform: (document, user) => {
+    user.id = user._id.toString();
+    delete user._id;
+    delete user.__v;
+    delete user.password;
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
